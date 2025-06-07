81d07d81498e8cb2c0dc8211c5d6da866f832539
SantiQ0905 <santy.10qm.gm@gmail.com>
2025-06-07 13:36:43 -0600

    Initial Commit

---
+++ NewVisionApp/NewVisionAppApp.swift
@@ -0,0 +1,33 @@
+//
+//  NewVisionAppApp.swift
+//  NewVisionApp
+//
+//  Created by Alumno on 07/06/25.
+//
+
+import SwiftUI
+
+@main
+struct NewVisionAppApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
