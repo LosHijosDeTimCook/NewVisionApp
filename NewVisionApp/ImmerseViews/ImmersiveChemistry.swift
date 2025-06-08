589208a5f3f5f9c69085136b26616e35a4ffcb72
FabricioBanda <fabribanda438@gmail.com>
2025-06-08 04:35:58 -0600

    ULTIMA VERSION ( PARA RIGO ) ❤️

---
+++ NewVisionApp/ImmerseViews/ImmersiveChemistry.swift
@@ -0,0 +1,18 @@
+//
+//  ImmersiveChemistry.swift
+//  NewVisionApp
+//
+//  Created by Alumno on 08/06/25.
+//
+
+import SwiftUI
+
+struct ImmersiveChemistry: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImmersiveChemistry()
+}
---
+++ NewVisionApp/MessageLevelPhysics.swift
@@ -0,0 +1,18 @@
+//
+//  MessageLevelPhysics.swift
+//  NewVisionApp
+//
+//  Created by Alumno on 08/06/25.
+//
+
+import SwiftUI
+
+struct MessageLevelPhysics: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessageLevelPhysics()
+}
