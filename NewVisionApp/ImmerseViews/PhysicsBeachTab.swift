109d74670d6dfcfc8e07b8cc9fb529a181132943
FabricioBanda <fabribanda438@gmail.com>
2025-06-08 07:51:46 -0600

    ULTIMO ANTES DE INTEGRAR NIVEL MATEMATICAS

---
+++ NewVisionApp/ImmerseViews/PhysicsBeachTab.swift
@@ -0,0 +1,23 @@
+//
+//  PhysicsBeachTab.swift
+//  NewVisionApp
+//
+//  Created by Rigoberto Said Soto Quiroga on 08/06/25.
+//
+
+import SwiftUI
+
+struct PhysicsBeachTab: View {
+    var body: some View {
+        VStack {
+            Text("Aquí va la info de Beach")
+                .font(.title)
+        }
+    }
+}
+
+
+
+#Preview {
+    PhysicsBeachTab()
+}
