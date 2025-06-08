396b0c66ad44e6c67deee90c77864a6cc607995a
FabricioBanda <fabribanda438@gmail.com>
2025-06-08 01:31:36 -0600

    Actualizacion : Vista Inicial , Menu

---
+++ NewVisionApp/MenuViews/QuimicaView.swift
@@ -0,0 +1,18 @@
+//
+//  QuimicaView.swift
+//  NewVisionApp
+//
+//  Created by Alumno on 07/06/25.
+//
+
+import SwiftUI
+
+struct QuimicaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuimicaView()
+}
